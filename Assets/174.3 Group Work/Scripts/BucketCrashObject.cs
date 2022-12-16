cf558ccbc4915ccaa55cd6a3cc6582b65b6177fb
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2022-12-16 12:00:54 +1100

    Adding Audio to my models

---
+++ Assets/174.3 Group Work/Scripts/BucketCrashObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class inherits from TargetObject and represents a CrashObject.
+/// </summary>
+public class BucketCrashObject : TargetObject
+{
+    [Header("CrashObject")]
+    [Tooltip("The VFX prefab spawned when the object is collected")]
+    public ParticleSystem CollectVFX;
+
+    [Tooltip("The position of the centerOfMass of this rigidbody")]
+    public Vector3 centerOfMass;
+
+    [Tooltip("Apply a force to the crash object to make it fly up onTrigger")]
+    public float forceUpOnCollide;
+
+    Rigidbody m_rigid;
+    public AudioClip bucket;
+    public AudioSource source;
+
+    void Start()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+        m_rigid.centerOfMass = centerOfMass;
+        Register();
+    }
+
+    void OnCollect(Collider other)
+    {
+        active = false;
+        if (CollectVFX)
+            CollectVFX.Play();
+               
+        if (m_rigid) m_rigid.AddForce(forceUpOnCollide*Vector3.up, ForceMode.Impulse);
+        
+        Objective.OnUnregisterPickup(this);
+
+        TimeManager.OnAdjustTime(TimeGained);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!active) return;
+        
+        if ((layerMask.value & 1 << other.gameObject.layer) > 0 && other.gameObject.CompareTag("Player"))
+            OnCollect(other);
+
+        source.PlayOneShot(bucket);
+    }
+}
